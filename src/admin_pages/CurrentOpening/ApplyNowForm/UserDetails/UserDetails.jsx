63dc3558ca82a8d1e8405d22dc36987c9bdcdb1b
rahul.gandhi@cviewsurvey.com <rahul.gandhi@cviewsurvey.com>
2024-01-18 00:02:21 -0800

    final merge change 18/01

--- src/admin_pages/CurrentOpening/ApplyNowForm/UserDetails/UserDetails.jsx
+++ src/admin_pages/CurrentOpening/ApplyNowForm/UserDetails/UserDetails.jsx
@@ -12,15 +12,11 @@ import "./UserDetails.css";
 import apiService from "../../../../Services/ApiServices";
 function UserDetails({formValues, setFormValues}) {
   const [posts, setPosts] = useState([]);
+  const [selectedPost, setSelectedPost] = useState('');
   const [departments, setDepartments] = useState([]);
-  const [selectedDepartment, setSelectedDepartment] = useState(null);
-  const [selectedPost, setSelectedPost] = useState(null);
-  const [maritalStatus, setMaritalStatus] = useState("");
-  const [selectedOption, setSelectedOption] = useState("");
-  const [otherValue, setOtherValue] = useState("");
+  const [selectedDepartment, setSelectedDepartment] = useState('');
+  const [maritalStatus, setMaritalStatus] = useState('');
 
-  const handleDropdownChange = (event) => {
-    const selectedValue = event.target.value;
 
   // const [formValues, setFormValues] = useState({
   //   email: '',
@@ -44,8 +40,6 @@ function UserDetails({formValues, setFormValues}) {
   //   department_master_id: '',
   //   pin_code: '',
 
-    setSelectedOption(selectedValue);
-  };
 
 
   // });
@@ -57,7 +51,7 @@ function UserDetails({formValues, setFormValues}) {
         // Update the state with the fetched data
         setPosts(response.data);
       })
-      .catch((error) => console.error("Error fetching data:", error));
+      .catch(error => console.error('Error fetching data:', error));
   }, []);
 
   useEffect(() => {
@@ -66,7 +60,7 @@ function UserDetails({formValues, setFormValues}) {
         const response = await apiService.getDepartments();
         setDepartments(response.data);
       } catch (error) {
-        console.error("Error fetching departments:", error);
+        console.error('Error fetching departments:', error);
       }
     };
 
@@ -89,7 +83,7 @@ function UserDetails({formValues, setFormValues}) {
       }));
     
     } else {
-      console.error("Selected department not found");
+      console.error('Selected department not found');
     }
   };
 
@@ -108,9 +102,10 @@ function UserDetails({formValues, setFormValues}) {
         }
       }));
     } else {
-      console.error("Selected post not found");
+      console.error('Selected post not found');
     }
   };
+
 
   const handleMaritalStatusChange = (event) => {
     const selectedMaritalStatus = event.target.value;
@@ -141,7 +136,7 @@ function UserDetails({formValues, setFormValues}) {
     <>
       <form method="post" >
         <div className="container">
-         <div style={{ marginTop: "20px" }}>
+          <div style={{ marginTop: "20px" }}>
             <div>
               <h5 className="UD-heading">Personal Details</h5>
               <p className="UD-subheading">
