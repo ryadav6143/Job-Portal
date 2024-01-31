a77a15cb743b5ee21f17719b525eaf5c2d12928e
rahul.gandhi@cviewsurvey.com <rahul.gandhi@cviewsurvey.com>
2024-01-31 14:52:22 +0530

    add exist email or  contact functionality in apply now

--- src/candidate_pages/CandidateLogin/CandidateLogin/ForgottenPassword.jsx
+++ src/candidate_pages/CandidateLogin/CandidateLogin/ForgottenPassword.jsx
@@ -2,39 +2,46 @@ import React, { useState } from "react";
 import { faEyeSlash, faEye } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import logo from "../../../assets/logos/logo.png";
-import Modal from '@mui/material/Modal';
-import Box from '@mui/material/Box';
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import close from "../../../assets/logos/close.png";
+import "./CandidateLogin.css";
+import Backdrop from "@mui/material/Backdrop";
 
-import { FormControl } from '@mui/material';
-
+import { FormControl } from "@mui/material";
 
 function ForgottenPassword() {
-    const [showPassword, setShowPassword] = useState(false);
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-    const handleTogglePassword = () => {
-        setShowPassword(!showPassword);
-      };
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
 
-      const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 400,
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 4,
-      };
-      
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 310,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const handleXButtonClick = () => {
+    // Close the modal when 'x' button is clicked
+    setOpen(false);
+  };
+
   return (
-   <>
+    <>
       <div className="login-container">
         <div className="logo-section">
           <img className="logo-img" src={logo} alt="Logo" />
@@ -43,8 +50,7 @@ function ForgottenPassword() {
           <p className="login-content">Forgotten Password</p>
         </div>
 
-     
-        <form  className="login-form" style={{height: "375px"}}>
+        <form className="login-form" style={{ height: "375px" }}>
           <label htmlFor="username">Email:</label>
           <input
             type="email"
@@ -71,7 +77,6 @@ function ForgottenPassword() {
             </span>
           </div>
 
-
           <label htmlFor="password"> Confirm Password:</label>
           <div className="password-input-container">
             <input
@@ -79,7 +84,7 @@ function ForgottenPassword() {
               id="password"
               name=""
               // value=""
-           
+
               required
               className="password-input"
             />
@@ -90,36 +95,45 @@ function ForgottenPassword() {
 
           <div className="btn-login">
             <button type="submit" onClick={handleOpen} className="login-button">
-           Submit
+              Submit
             </button>
           </div>
           <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-        <FormControl>
-          <div >
-          <form >
-            <label className="UD-SetLabel-Name">Enter OTP</label>
-<input   type="text" name="" id="" />
-<button type="button" class="btn btn-success">Ok</button>
+             open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+      BackdropComponent={(props) => (
+        <Backdrop {...props} invisible onClick={() => {}} />
+      )}
+          >
+            <Box sx={style}>
+              <FormControl>
+                <div>
+                  <form>
+                    <img
+                      onClick={handleXButtonClick}
+                      className="close-btn"
+                      src={close}
+                    />
+                    <label className="UD-SetLabel-Name">Enter OTP</label>
 
-          </form>
-          </div>  
-
-</FormControl>
-        </Box>
-      </Modal>
+                    <input type="text" name="" id="" />
+                    <button type="button" class="btn btn-success">
+                      Submit
+                    </button>
+                  </form>
+                </div>
+              </FormControl>
+            </Box>
+          </Modal>
         </form>
         <div className="design-content">
           <p>Design & Developed By Corus View</p>
         </div>
       </div>
-   </>
-  )
+    </>
+  );
 }
 
-export default ForgottenPassword
+export default ForgottenPassword;
