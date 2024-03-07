7254eba7bc18032907a7f5c8fc2fe674be4902b1
rahul.gandhi@cviewsurvey.com <rahul.gandhi@cviewsurvey.com>
2024-03-07 18:22:36 +0530

    admin report changes 07/03

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 // export const BASE_URL = "http://192.168.1.8:8090/v1/api";
-export const CANDIDATE_BASE_URL = "http://192.168.1.8:8090/v1/api";
+export const CANDIDATE_BASE_URL = "http://192.168.1.18:8090/v1/api";
 
-export const ADMIN_BASE_URL = "http://192.168.1.8:8090/v1/api";
+export const ADMIN_BASE_URL = "http://192.168.1.18:8090/v1/api";
