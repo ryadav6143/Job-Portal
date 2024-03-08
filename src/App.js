fdf81fb5a17a4409310f77e86e923a7aa780ee32
rahul.gandhi@cviewsurvey.com <rahul.gandhi@cviewsurvey.com>
2024-03-08 18:50:27 +0530

    done private routing 08/03/2024

--- src/App.js
+++ src/App.js
@@ -1,28 +1,5 @@
 import React from "react";
 import { BrowserRouter as Main, Route, Routes } from "react-router-dom";
-
-
-
-
-import Administrative from "./admin_pages/JobProfile/Administrative/Administrative";
-import Research from "./admin_pages/JobProfile/Research/Research";
-import Technical from "./admin_pages/JobProfile/Technical/Technical";
-import ApplyNow from "./admin_pages/CurrentOpening/ApplyNowForm/ApplyNow";
-
-
-
-import ContactUs from "./admin_pages/Contactus/ContactUs";
-import Submitsuccess from "./admin_pages/DropCV/OTPVerifivation/Submitsuccess";
-import OTPVerification from "./admin_pages/DropCV/OTPVerifivation/OTPVerification";
-import TestPages from "./admin_pages/TestPages/TestPages";
-import AddOpenings from "./admin_pages/AdminLogin/AdminChildPages/MasterCurrentOpening/AddOpeningForm/AddOpenings";
-
-
-import PageNotFound from "./admin_pages/PageNotFound/PageNotFound";
-import EditOpenings from "./admin_pages/AdminLogin/AdminChildPages/MasterCurrentOpening/EditOpeningForm/EditOpenings";
-import RegisterAdmin from "./admin_pages/AdminRegister/RegisterAdmin";
-import Sidenav from "./admin_pages/TestPages/Sidenav";
-import Notification from "./Notification/Notification";
 import UnAuthRoutes from "./routes/UnAuthRoutes";
 import CandidateAuthRoutes from "./routes/CandidateAuthRoutes";
 import AdminAuthRoutes from "./routes/AdminAuthRoutes";
@@ -34,37 +11,6 @@ function App() {
           {UnAuthRoutes}
           {CandidateAuthRoutes}
           {AdminAuthRoutes}
-          {/* <Route path="login" element={<Login />}></Route> */}
-          
-         
-          <Route
-            path="administrative-jobs"
-            element={<Administrative />}
-          ></Route>
-          <Route path="research-jobs" element={<Research />}></Route>
-          <Route path="technical-jobs" element={<Technical />}></Route>
-          <Route path="apply-now" element={<ApplyNow />}></Route>
-         
-      
-          {/* <Route path="forgetpassword" element={<ForgottenPassword />}></Route> */}
-
-          
-
-        
-          <Route path="contact-us" element={<ContactUs />}></Route>
-          <Route path="submit" element={<Submitsuccess />}></Route>
-          <Route path="verify" element={<OTPVerification />}></Route>
-          <Route
-            path="verification-successfull"
-            element={<Submitsuccess />}
-          ></Route>
-          <Route path="add-openings" element={<AddOpenings />}></Route>
-          <Route path="edit-openings/:id" element={<EditOpenings />}></Route>
-          <Route path="test" element={<TestPages />}></Route>
-          <Route path="admin_register" element={<RegisterAdmin />}></Route>
-          <Route path="sidenav" element={<Sidenav />}></Route>
-
-          <Route path="*" element={<PageNotFound />}></Route>
         </Routes>
       </Main>
     </>
--- src/routes/UnAuthRoutes.js
+++ src/routes/UnAuthRoutes.js
@@ -17,24 +17,129 @@ import CurrentExperience from "../admin_pages/DropCV/CurrentExperience/CurrentEx
 import UserDetails from "../admin_pages/CurrentOpening/ApplyNowForm/UserDetails/UserDetails";
 import Academics from "../admin_pages/JobProfile/Academics/Academics";
 import PersonalDetails from "../admin_pages/DropCV/PersonalDetails/PersonalDeatils";
-console.log("inside UnAuthRoutes")
-const UnAuthRoutes =[
-    <Route exact path="/" element={<UnAuthGuards component ={<Home/>} />}></Route>,
-    <Route path="/current-opening" element={<UnAuthGuards component ={<CurrentOpening/>} />}></Route>,
-    <Route path="/job-profile" element={<UnAuthGuards component ={<JobProfile/>} />}></Route>,
-    <Route path="/interview-schedule" element={<UnAuthGuards component ={<InterviewShedule/>} />}></Route>,
-    <Route path="/faq-section" element={<UnAuthGuards component = {<FAQ />} />}  ></Route>,
-    <Route path="/drop-cv" element={<UnAuthGuards component = {<DropCV />}/>}></Route>,
-    <Route path="/forgetpassword" element= {<UnAuthGuards component = {<ForgottenPassword />}/>}   ></Route>,
-    <Route path="/candidate-login" element={<UnAuthGuards component ={<CandidateLogin />} />} ></Route>,
-    <Route path="/admin-login" element= {<UnAuthGuards component ={<Login />} />}></Route>,
-    <Route path="personaldetails" element= {<UnAuthGuards component ={<PersonalDetails />} />}  ></Route>,
-    <Route path="qualification" element=   {<Qualification />}></Route>,
-    <Route path="currentexperience"  element= {<UnAuthGuards component ={<CurrentExperience />} />}   ></Route>,
-    <Route path="userdetails" element= {<UnAuthGuards component ={<UserDetails />} />}  ></Route>,
-    <Route path="academics-jobs" element= {<UnAuthGuards component = {<Academics />} />} ></Route>,
+import Administrative from "../admin_pages/JobProfile/Administrative/Administrative";
+import Research from "../admin_pages/JobProfile/Research/Research";
+import Technical from "../admin_pages/JobProfile/Technical/Technical";
+import ApplyNow from "../admin_pages/CurrentOpening/ApplyNowForm/ApplyNow";
+import EditOpenings from "../admin_pages/AdminLogin/AdminChildPages/MasterCurrentOpening/EditOpeningForm/EditOpenings";
+import RegisterAdmin from "../admin_pages/AdminRegister/RegisterAdmin"
+import PageNotFound from "../admin_pages/PageNotFound/PageNotFound";
+import ContactUs from "../admin_pages/Contactus/ContactUs";
+import Submitsuccess from "../admin_pages/DropCV/OTPVerifivation/Submitsuccess";
+import OTPVerification from "../admin_pages/DropCV/OTPVerifivation/OTPVerification";
+import TestPages from "../admin_pages/TestPages/TestPages";
+import AddOpenings from "../admin_pages/AdminLogin/AdminChildPages/MasterCurrentOpening/AddOpeningForm/AddOpenings";
 
+console.log("inside UnAuthRoutes");
+const UnAuthRoutes = [
+  <Route
+    exact
+    path="/"
+    element={<UnAuthGuards component={<Home />} />}
+  ></Route>,
+  <Route
+    path="/current-opening"
+    element={<UnAuthGuards component={<CurrentOpening />} />}
+  ></Route>,
+  <Route
+    path="/job-profile"
+    element={<UnAuthGuards component={<JobProfile />} />}
+  ></Route>,
+  <Route
+    path="/interview-schedule"
+    element={<UnAuthGuards component={<InterviewShedule />} />}
+  ></Route>,
+  <Route
+    path="/faq-section"
+    element={<UnAuthGuards component={<FAQ />} />}
+  ></Route>,
+  <Route
+    path="/drop-cv"
+    element={<UnAuthGuards component={<DropCV />} />}
+  ></Route>,
+  <Route
+    path="/forgetpassword"
+    element={<UnAuthGuards component={<ForgottenPassword />} />}
+  ></Route>,
+  <Route
+    path="/candidate-login"
+    element={<UnAuthGuards component={<CandidateLogin />} />}
+  ></Route>,
+  <Route
+    path="/admin-login"
+    element={<UnAuthGuards component={<Login />} />}
+  ></Route>,
+  <Route
+    path="personaldetails"
+    element={<UnAuthGuards component={<PersonalDetails />} />}
+  ></Route>,
+  <Route path="qualification" element={<Qualification />}></Route>,
+  <Route
+    path="currentexperience"
+    element={<UnAuthGuards component={<CurrentExperience />} />}
+  ></Route>,
+  <Route
+    path="userdetails"
+    element={<UnAuthGuards component={<UserDetails />} />}
+  ></Route>,
+  <Route
+    path="academics-jobs"
+    element={<UnAuthGuards component={<Academics />} />}
+  ></Route>,
+  <Route
+    path="administrative-jobs"
+    element={<UnAuthGuards component={<Administrative />} />}
+  ></Route>,
+  <Route
+    path="research-jobs"
+    element={<UnAuthGuards component={<Research />} />}
+  ></Route>,
+  <Route
+    path="technical-jobs"
+    element={<UnAuthGuards component={<Technical />} />}
+  ></Route>,
+  <Route
+    path="apply-now"
+    element={<UnAuthGuards component={<ApplyNow />} />}
+  ></Route>,
 
-]
+  <Route
+    path="contact-us"
+    element={<UnAuthGuards component={<ContactUs />} />}
+  ></Route>,
+  <Route
+    path="submit"
+    element={<UnAuthGuards component={<Submitsuccess />} />}
+  ></Route>,
+  <Route
+    path="verify"
+    element={<UnAuthGuards component={<OTPVerification />} />}
+  ></Route>,
+  <Route
+    path="verification-successfull"
+    element={<UnAuthGuards component={<Submitsuccess />} />}
+  ></Route>,
+  <Route
+    path="add-openings"
+    element={<UnAuthGuards component={<AddOpenings />} />}
+  ></Route>,
+  <Route
+    path="edit-openings/:id"
+    element={<UnAuthGuards component={<EditOpenings />} />}
+  ></Route>,
+  <Route
+    path="test"
+    element={<UnAuthGuards component={<TestPages />} />}
+  ></Route>,
+  <Route
+    path="admin_register"
+    element={<UnAuthGuards component={<RegisterAdmin />} />}
+  ></Route>,
 
-export default UnAuthRoutes
+  <Route
+    path="*"
+    element={<UnAuthGuards component={<PageNotFound />} />}
+  ></Route>,
+];
+
+export default UnAuthRoutes;
