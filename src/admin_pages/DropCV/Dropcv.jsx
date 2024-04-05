597a6ffced9eccfb37108290b4b806b0a11a4d07
rahul.gandhi@cviewsurvey.com <rahul.gandhi@cviewsurvey.com>
2024-04-05 13:10:17 +0530

    remove error from drop cv 05/04/2024

--- src/admin_pages/DropCV/Dropcv.jsx
+++ src/admin_pages/DropCV/Dropcv.jsx
@@ -458,7 +458,7 @@ function Dropcv() {
                 stepProps.completed = false;
               }
               return (
-                <Step key={label} {...stepProps}>
+                <Step key={`step-${index}`}>
                   <StepLabel {...labelProps}>{label}</StepLabel>
                 </Step>
               );
