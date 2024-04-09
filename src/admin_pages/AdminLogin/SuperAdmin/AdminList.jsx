ce8725a7dd038f5f47a0d2c17d1d1f0655926cdc
rahul.gandhi@cviewsurvey.com <rahul.gandhi@cviewsurvey.com>
2024-04-09 16:12:23 +0530

    09/04/2024

--- src/admin_pages/AdminLogin/SuperAdmin/AdminList.jsx
+++ src/admin_pages/AdminLogin/SuperAdmin/AdminList.jsx
@@ -102,8 +102,8 @@ function AdminList() {
     left: "50%",
     transform: "translate(-50%, -50%)",
     width: "40%", 
-    maxWidth: "600px", 
-    height: "72%", 
+    
+    height: "520px", 
     maxHeight: "600px", 
     bgcolor: "background.paper",
     border: "2px solid #000",
